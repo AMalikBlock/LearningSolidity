4922bfceaa8a65afb75c578def34397b27c7390c
Abdullah Malik <AMalikBlock@users.noreply.github.com>
2023-09-25 17:33:43 +0100

    File; working with msg.sender to call address

---
+++ msgSender.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.16;
+
+//working with (msg.sender)
+
+contract exampleMsgSender {
+
+       address public someAddress; //store address 
+
+             // gets sender address (msg.sender)
+              function msgSender () public {
+                
+                   someAddress = msg.sender;
+
+                }
+        
+        
+}
